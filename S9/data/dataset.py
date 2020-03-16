ea25392e6a1b33d2a758dbcb685c8cd68f4a7b02
Shantanu <thegeek.004@gmail.com>
2020-03-17 02:56:52 +0530

    Update library code and add GRAD-CAM

--- S9/data/dataset.py
+++ S9/data/dataset.py
@@ -1,16 +1,18 @@
+import torch
 import numpy as np
 
 from data.downloader import download_cifar10
-from data.processing import transformations, data_loader
+from data.processing import Transformations, data_loader
+from data.utils import unnormalize
 
 
 class CIFAR10:
     """ Load CIFAR-10 Dataset. """
 
     def __init__(
-        self, train_batch_size=1, val_batch_size=1,
-        cuda=False, num_workers=1, path=None,
-        horizontal_flip=0.0, vertical_flip=0.0, rotation=0.0, random_erasing=0.0
+        self, train_batch_size=1, val_batch_size=1, cuda=False,
+        num_workers=1, path=None, horizontal_flip_prob=0.0,
+        vertical_flip_prob=0.0, rotate_degree=0.0, cutout=0.0
     ):
         """Initializes the dataset for loading.
 
@@ -22,13 +24,15 @@ class CIFAR10:
             path: Path where dataset will be downloaded. Defaults to None.
                 If no path provided, data will be downloaded in a pre-defined
                 directory.
-            horizontal_flip: Probability of an image being horizontally flipped.
+            horizontal_flip_prob: Probability of an image being horizontally flipped.
                 Defaults to 0.
-            vertical_flip: Probability of an image being vertically flipped.
+            vertical_flip_prob: Probability of an image being vertically flipped.
                 Defaults to 0.
-            rotation: Angle of rotation for image augmentation.
+            rotate_prob: Probability of an image being rotated.
                 Defaults to 0.
-            random_erasing: Probability that random erase will be performed.
+            rotate_degree: Angle of rotation for image augmentation.
+                Defaults to 0.
+            cutout: Probability that cutout will be performed.
                 Defaults to 0.
         """
         
@@ -45,18 +49,24 @@ class CIFAR10:
         )
 
         # Set data augmentation parameters
-        self.horizontal_flip = horizontal_flip
-        self.vertical_flip = vertical_flip
-        self.rotation = rotation
-        self.random_erasing = random_erasing
+        self.horizontal_flip_prob = horizontal_flip_prob
+        self.vertical_flip_prob = vertical_flip_prob
+        self.rotate_degree = rotate_degree
+        self.cutout = cutout
 
-        # Set transforms
-        self.train_transform = self._transform()
+        # Set mean and standard deviation
+        self.mean = (0.5, 0.5, 0.5)
+        self.std = (0.5, 0.5, 0.5)
+
+        # Set validation data
+        # Validation data is fetched first in order to
+        # get the image size which will be needed in cutout
         self.val_transform = self._transform(train=False)
-
-        # Download dataset
-        self.train_data = self._download()
         self.val_data = self._download(train=False)
+
+        # Set training data
+        self.train_transform = self._transform()
+        self.train_data = self._download()
     
     def _transform(self, train=True):
         """Define data transformations
@@ -68,12 +78,23 @@ class CIFAR10:
         Returns:
             Returns data transforms based on the training mode.
         """
-        return transformations(
-            horizontal_flip=self.horizontal_flip,
-            vertical_flip=self.vertical_flip,
-            rotation=self.rotation,
-            random_erasing=self.random_erasing
-        ) if train else transformations()
+
+        args = {
+            'mean': self.mean,
+            'std': self.std,
+            'train': False
+        }
+
+        if train:
+            args['train'] = True
+            args['horizontal_flip_prob'] = self.horizontal_flip_prob
+            args['vertical_flip_prob'] = self.vertical_flip_prob
+            args['rotate_degree'] = self.rotate_degree
+            args['cutout'] = self.cutout
+            args['cutout_height'] = self.image_size[0] // 2
+            args['cutout_width'] = self.image_size[1] // 2
+
+        return Transformations(**args)
     
     def _download(self, train=True):
         """Download dataset.
@@ -87,6 +108,7 @@ class CIFAR10:
         transform = self.train_transform if train else self.val_transform
         return download_cifar10(self.path, train=train, transform=transform)
     
+    @property
     def classes(self):
         """ Return list of classes in the dataset. """
         return self.class_values
@@ -103,9 +125,23 @@ class CIFAR10:
         data = self.train_data if train else self.val_data
         return data.data, data.targets
     
+    @property
     def image_size(self):
         """ Return shape of data i.e. image size. """
-        return np.transpose(self.data()[0][0], (2, 0, 1)).shape
+        return np.transpose(self.data(train=False)[0][0], (2, 0, 1)).shape
+    
+    def unnormalize(self, image):
+        """Un-normalize a given image.
+
+        Args:
+            image: A 3-D ndarray or 3-D tensor.
+                If tensor, it should be in CPU.
+        """
+
+        if type(image) == torch.Tensor:
+            image = np.transpose(image.clone().numpy(), (1, 2, 0))
+
+        return unnormalize(image, self.mean, self.std)
     
     def loader(self, train=True):
         """Create data loader.
--- S9/data/processing.py
+++ S9/data/processing.py
@@ -1,48 +1,76 @@
+import numpy as np
 import torch
-from torchvision import transforms
+import albumentations as A
+from albumentations.pytorch import ToTensor
 
 
-def transformations(horizontal_flip=0.0, vertical_flip=0.0, rotation=0.0, random_erasing=0.0):
-    """Create data transformations
+class Transformations:
+    """ Wrapper class to pass on albumentaions transforms into PyTorch. """
+
+    def __init__(
+        self, horizontal_flip_prob=0.0, vertical_flip_prob=0.0,
+        rotate_degree=0.0, cutout=0.0, cutout_height=0, cutout_width=0,
+        mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5), train=True
+    ):
+        """Create data transformation pipeline
+        
+        Args:
+            horizontal_flip_prob: Probability of an image being horizontally flipped.
+                Defaults to 0.
+            vertical_flip_prob: Probability of an image being vertically flipped.
+                Defaults to 0.
+            rotate_degree: Angle of rotation for image augmentation.
+                Defaults to 0.
+            cutout: Probability that cutout will be performed.
+                Defaults to 0.
+            cutout_height: Max height of the cutout box.
+                Defaults to 0.
+            cutout_width: Max width of the cutout box.
+                Defaults to 0.
+            mean: Mean value. Defaults to 0.5 for each channel.
+            std: Standard deviation value. Defaults to 0.5 for each channel.
+        """
+        transforms_list = []
+
+        if train:
+            transforms_list += [
+                A.HorizontalFlip(p=horizontal_flip_prob),  # Horizontal Flip
+                A.VerticalFlip(p=vertical_flip_prob),  # Vertical Flip
+                A.Rotate(limit=rotate_degree),  # Rotate image
+
+                # CutOut
+                A.CoarseDropout(
+                    p=cutout, max_holes=1, fill_value=tuple(255 * x for x in mean),
+                    max_height=cutout_height, max_width=cutout_width, min_height=1, min_width=1
+                )
+            ]
+        
+        transforms_list += [
+            # normalize the data with mean and standard deviation to keep values in range [-1, 1]
+            # since there are 3 channels for each image,
+            # we have to specify mean and std for each channel
+            A.Normalize(mean=mean, std=std, always_apply=True),
+
+            # convert the data to torch.FloatTensor
+            # with values within the range [0.0 ,1.0]
+            ToTensor()
+        ]
+
+        self.transform = A.Compose(transforms_list)
     
-    Args:
-        horizontal_flip: Probability of an image being horizontally flipped.
-            Defaults to 0.
-        vertical_flip: Probability of an image being vertically flipped.
-            Defaults to 0.
-        rotation: Angle of rotation for image augmentation.
-            Defaults to 0.
-        random_erasing: Probability that random erase will be performed.
-            Defaults to 0.
-    
-    Returns:
-        Transform object containing defined data transformations.
-    """
+    def __call__(self, image):
+        """Process and image through the data transformation pipeline.
 
-    transforms_list = []
+        Args:
+            image: Image to process.
+        
+        Returns:
+            Transformed image.
+        """
 
-    if horizontal_flip > 0:  # Horizontal Flip
-        transforms_list += [transforms.RandomHorizontalFlip(horizontal_flip)]
-    if vertical_flip > 0:  # Vertical Flip
-        transforms_list += [transforms.RandomVerticalFlip(vertical_flip)]
-    if rotation > 0:  # Rotate image
-        transforms_list += [transforms.RandomRotation(rotation, fill=1)]
-    
-    transforms_list += [
-        # convert the data to torch.FloatTensor
-        # with values within the range [0.0 ,1.0]
-        transforms.ToTensor(),
-
-        # normalize the data with mean and standard deviation to keep values in range [-1, 1]
-        # since there are 3 channels for each image,
-        # we have to specify mean and std for each channel
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-    ]
-
-    if random_erasing > 0:
-        transforms_list += [transforms.RandomErasing(random_erasing)]
-    
-    return transforms.Compose(transforms_list)
+        image = np.array(image)
+        image = self.transform(image=image)['image']
+        return image
 
 
 def data_loader(data, shuffle=True, batch_size=1, num_workers=1, cuda=False):
